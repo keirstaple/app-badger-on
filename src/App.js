bbcf8be99b2cb2e37d368a6ae621dd414a11b270
Keir Staple <keirstaple@comcast.net>
2018-01-06 19:18:53 -0800

    finish setting up hello world/project structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 
 const App = () => (
   <div className="App">
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
       <h1 className="App-title">Welcome to React</h1>
     </header>
     <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
+      Hello World!
     </p>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 /* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import store from './store';
 import App from './App';
+
+import './styles/index.scss';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route path="/" component={App}>
+        {/*<Route path="foo" component={Foo}/>*/}
+        {/*<Route path="bar" component={Bar}/>*/}
+      </Route>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
