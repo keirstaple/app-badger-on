7034b165012b0772ec67a97febfbf0aa174559e9
Keir Staple <keirstaple@comcast.net>
2018-02-12 21:25:08 -0800

    add test-watch and fix navBar filename

--- test/state/reducers/navBar.test.js
+++ test/state/reducers/navBar.test.js
@@ -1,13 +1,15 @@
 import { expect } from 'chai';
 
 import { navBar, INITIAL_STATE } from '../../../src/state/reducers/navBar';
-import { navBarOpen } from '../../../src/consts/navBar';
+import { navBarOpen, navBarClosed } from '../../../src/consts/navBar';
 import UPDATE_NAV_BAR_STATUS from '../../../src/actionTypes/navBar';
 
 describe('navBar actions', () => {
     it('should update the navBar status', () => {
         const action = { type: UPDATE_NAV_BAR_STATUS, status: navBarOpen };
         const results = { navBarStatus: navBarOpen };
-        expect(navBar(INITIAL_STATE, action)).toEqual(results);
+        const actionCreator = navBar(INITIAL_STATE, action);
+        expect(actionCreator).to.eql(results);
+        expect(actionCreator.navBarStatus).to.not.eql(navBarClosed);
     });
 });
