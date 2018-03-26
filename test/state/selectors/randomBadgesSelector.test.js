cda9bfdf0ca12806dba8ac37b8be03f42fecc180
Keir Staple <keirstaple@comcast.net>
2018-03-25 23:39:45 -0700

    refactor out magic number of random badges

--- test/state/selectors/randomBadgesSelector.test.js
+++ test/state/selectors/randomBadgesSelector.test.js
@@ -1,16 +1,17 @@
 import { expect } from 'chai';
 import uniq from 'lodash/uniq';
 
+import RANDOM_BADGE_COUNT from '../../../src/consts/randomBadges';
 import randomBadgesSelector from '../../../src/state/selectors/randomBadgesSelector';
 import mockState from '../../mocks/mockState';
 
 describe('randomBadgesSelector', () => {
   const randomBadges = randomBadgesSelector(mockState);
-  it('should return 3 badges', () => {
-    expect(randomBadges.length).to.equal(3);
+  it(`should return ${RANDOM_BADGE_COUNT} badges`, () => {
+    expect(randomBadges.length).to.equal(RANDOM_BADGE_COUNT);
   });
-  it('should return 3 badges that are not equal to one another', () => {
+  it(`should return ${RANDOM_BADGE_COUNT} badges that are not equal to one another`, () => {
     const uniqueCheck = uniq(randomBadges);
-    expect(uniqueCheck.length).to.equal(3);
+    expect(uniqueCheck.length).to.equal(RANDOM_BADGE_COUNT);
   });
 });
--- test/views/containers/randomBadges.test.js
+++ test/views/containers/randomBadges.test.js
@@ -5,6 +5,8 @@ import { shallow } from 'enzyme';
 import { baseComponent as RandomBadges } from '../../../src/views/containers/randomBadges';
 import RandomBadgeTile from '../../../src/views/components/randomBadgeTile';
 import randomBadgesSelector from '../../../src/state/selectors/randomBadgesSelector';
+import RANDOM_BADGE_COUNT from '../../../src/consts/randomBadges';
+
 import mockState from '../../mocks/mockState';
 
 describe('RandomBadges', () => {
@@ -16,7 +18,7 @@ describe('RandomBadges', () => {
     expect(wrapper(defaultProps)).to.have.length(1);
     expect(wrapper(defaultProps).find('.random-badges-container').hasClass('random-badges-container')).to.equal(true);
   });
-  it('should render 3 random badge tiles', () => {
-    expect(wrapper(defaultProps).find(RandomBadgeTile).length).to.equal(3);
+  it(`should render ${RANDOM_BADGE_COUNT} random badge tiles`, () => {
+    expect(wrapper(defaultProps).find(RandomBadgeTile).length).to.equal(RANDOM_BADGE_COUNT);
   });
 });
