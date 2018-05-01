7fb3d3f278796a78e25cac70bb091750abd5c65c
Keir Staple <keirstaple@comcast.net>
2018-05-01 00:35:45 -0400

    wip mock badge workflow

--- src/state/selectors/utils/stateBadges.js
+++ src/state/selectors/utils/stateBadges.js
@@ -1,3 +1,5 @@
+import { badges as mockBadges } from '../../../mocks/mockBadges.json';
+
 // TODO: return cloudPersistedBadges first, and then if those are undefined after a while then
 // return locallyPersistedBadges with a last updated time stamp and a user notification.
 const locallyPersistedBadges = state => state.badges.locallyPersistedBadges;
@@ -5,4 +7,7 @@ const cloudPersistedBadges = state => state.badges.cloudPersistedBadges;
 
 const stateBadges = cloudPersistedBadges || locallyPersistedBadges;
 
-export default stateBadges;
+// TODO: change this to @mars package version of an env variable if needed for integration tests
+const badges = process.env.MOCK === 'mock' ? mockBadges : stateBadges;
+
+export default badges;
