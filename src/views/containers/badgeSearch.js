093d4d70787ba7ea8edee5c9c688596c7aebf8a9
Keir Staple <keirstaple@comcast.net>
2018-05-18 19:36:53 -0700

    change routes on enter key

--- src/views/containers/badgeSearch.js
+++ src/views/containers/badgeSearch.js
@@ -3,15 +3,18 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { Field, reduxForm, getFormValues } from 'redux-form';
-import { push } from 'react-router-redux';
+import { withRouter } from 'react-router';
+import getProp from 'lodash/fp/get';
 
 import BadgeSearchList from '../components/badgeSearchList';
 
-import filteredBadgesBySearchTerm from '../../state/selectors/filteredBadgesBySearchTerm';
+import stateBadges from '../../state/selectors/stateBadges';
+
+import filterBadgesBySearch from '../../utils/filterBadgesBySearch';
 
 import badgePropType from '../../consts/propTypes';
-import { BADGE_SEARCH_FORM, FORM_FIELDS } from '../../consts/form';
-import { rootSearch } from '../../consts/routeParams';
+import { BADGE_SEARCH_FORM, FORM_FIELDS, KEYBOARD } from '../../consts/form';
+import routeParams from '../../consts/routeParams';
 
 const { BADGE_FUZZY_SEARCH } = FORM_FIELDS;
 
@@ -19,54 +22,59 @@ class BadgeSearch extends Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
+    this.grabSearchTerm = this.grabSearchTerm.bind(this);
   }
-  componentWillReceiveProps(nextProps) {
-    if (this.props[BADGE_FUZZY_SEARCH] !== nextProps[BADGE_FUZZY_SEARCH]) {
-      nextProps.change(BADGE_FUZZY_SEARCH, nextProps[BADGE_FUZZY_SEARCH]);
+  handleChange(e) {
+    const { history } = this.props;
+    const { key, target: { value } } = e;
+    const uri = `${routeParams.rootSearch}${encodeURIComponent(value)}`;
+    this.props.change(BADGE_FUZZY_SEARCH, value);
+    if (key === KEYBOARD.ENTER) {
+      history.push(uri);
     }
   }
-  handleChange(e, value) {
-    const { pushURI } = this.props;
-    pushURI(`${rootSearch}${encodeURIComponent(value)}`);
+  grabSearchTerm() {
+    const { match } = this.props;
+    return getProp('params.searchTerm')(match);
   }
   render() {
-    const { searchedBadges } = this.props;
+    const { badges } = this.props;
+    const searchedBadges = filterBadgesBySearch(badges, this.grabSearchTerm());
     return (
       <div className="badge-search-container">
         <Field
           name={BADGE_FUZZY_SEARCH}
           component="input"
           type="text"
-          onChange={this.handleChange}
+          onKeyPress={this.handleChange}
         />
-        <BadgeSearchList searchedBadges={searchedBadges} />
+        {searchedBadges && <BadgeSearchList searchedBadges={searchedBadges} />}
       </div>
     );
   }
 }
 
-BadgeSearch.defaultProps = {
-  searchedBadges: [],
-};
-
 BadgeSearch.propTypes = {
-  pushURI: PropTypes.func.isRequired,
+  badges: PropTypes.arrayOf(PropTypes.shape(badgePropType)).isRequired,
   change: PropTypes.func.isRequired,
-  searchedBadges: PropTypes.arrayOf(badgePropType),
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      searchTerm: PropTypes.string,
+    }),
+  }).isRequired,
 };
 
 export default compose(
+  withRouter,
   reduxForm({
     form: BADGE_SEARCH_FORM,
   }),
-  connect(
-    state => ({
-      values: getFormValues(BADGE_SEARCH_FORM)(state),
-      searchedBadges: filteredBadgesBySearchTerm(state),
-    }),
-    dispatch => ({
-      pushURI: uri => dispatch(push(uri)),
-    }),
-  ),
+  connect(state => ({
+    values: getFormValues(BADGE_SEARCH_FORM)(state),
+    badges: stateBadges(state),
+  })),
 )(BadgeSearch);
 
