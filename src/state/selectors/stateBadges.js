5efc673f3d392fc07d57fdc014c8804059b52125
Keir Staple <keirstaple@comcast.net>
2018-05-18 21:12:34 -0700

    check for empty cloud badges and now it works

--- src/state/selectors/stateBadges.js
+++ src/state/selectors/stateBadges.js
@@ -1,4 +1,6 @@
 import { createSelector } from 'reselect';
+import isEmpty from 'lodash/fp/isEmpty';
+
 import { badges as mockBadges } from '../../mocks/mockBadges.json';
 
 // TODO: return cloudPersistedBadges first, and then if those are undefined after a while then
@@ -6,11 +8,15 @@ import { badges as mockBadges } from '../../mocks/mockBadges.json';
 const cloudPersistedBadges = state => state.badges.cloudPersistedBadges;
 const locallyPersistedBadges = state => state.badges.locallyPersistedBadges;
 
-// TODO: This isn't actually working offline
 const stateBadges = createSelector(
   cloudPersistedBadges,
   locallyPersistedBadges,
-  (cloud, local) => (cloud || local),
+  (cloud, local) => {
+    if (!isEmpty(cloud)) {
+      return cloud;
+    }
+    return local;
+  },
 );
 
 // TODO: change this to @mars package version of an env variable if needed for integration tests
